c41a6087246539cf212d5ed1611ffb092e02a814
Adrian Tudev <tudev@chalmers.se>
2026-01-16 11:20:13 +0100

    refactor repl

--- src/main.c
+++ src/main.c
@@ -1,15 +1,4 @@
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-#include <readline/history.h>
-#include <readline/readline.h>
-
-// HACK: readline defines a `RETURN` macro that conflicts with a token
-#ifdef RETURN
-#undef RETURN
-#endif
 
 #include "lint.h"
 
@@ -18,15 +7,8 @@ int main(int argc, char* args[]) {
   if (argc == 2) {
     run_file(args[1]);
     return 0;
-  }
-
-  char* line;
-  while ((line = readline("> ")) != NULL) {
-    if (*line) {
-      add_history(line);
-      repl(line, 0);
-    }
-    free(line);
+  } else {
+    repl();
   }
 
   return 0;
