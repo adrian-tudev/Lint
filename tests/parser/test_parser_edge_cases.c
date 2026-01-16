21ba88a0cb4ba3e1f2e88f4b250d1713e9ffd796
Adrian Tudev <tudev@chalmers.se>
2026-01-16 23:40:25 +0100

    single identifier case test

--- tests/parser/test_parser_edge_cases.c
+++ tests/parser/test_parser_edge_cases.c
@@ -5,6 +5,35 @@
 bool parser_edge_cases(void) {
   bool passed_tests = true;
 
+  { // Single identifier statement
+    const char* source = "x;";
+    Vector tokens = tokenize(source, 0);
+    Program* program = parse(tokens);
+    
+    passed_tests &= test_assert(program != NULL, "Program should not be NULL");
+    if (program) {
+        passed_tests &= test_assert(program->items.size == 1, "Should parse single identifier statement");
+        if (program->items.size == 1) {
+            TopLevel* tl = (TopLevel*)vec_get(&program->items, 0);
+            passed_tests &= test_assert(tl->kind == TOP_STATEMENT, "Should be a statement");
+            if (tl->kind == TOP_STATEMENT) {
+                Statement* stmt = tl->as.statement;
+                passed_tests &= test_assert(stmt->kind == STMT_EXPR, "Should be an expression statement");
+                if (stmt->kind == STMT_EXPR) {
+                    Expression* expr = stmt->as.expr;
+                    passed_tests &= test_assert(expr->kind == EXPR_IDENTIFIER, "Should be an identifier expression");
+                    if (expr->kind == EXPR_IDENTIFIER) {
+                         passed_tests &= test_assert(strcmp(expr->as.identifier, "x") == 0, "Identifier should be 'x'");
+                    }
+                }
+            }
+        }
+    }
+
+    vec_free(&tokens);
+    program_free(program);
+  }
+
   { // Trailing binary operator (Crash reproduction)
     const char* source = "1 +";
     Vector tokens = tokenize(source, 0);
