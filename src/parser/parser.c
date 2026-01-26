17de0a9eeeb7165aebe6ee530e74ae51c2b25d32
Adrian Tudev <tudev@chalmers.se>
2026-01-26 22:13:25 +0100

    refactor main parse function

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,51 +8,47 @@
 #include "lexer/token.h"
 #include "utils/error.h"
 
+static bool try_parse(Program* program);
+
 // recursive descent parser
 Program* parse(Vector tokens) {
   init_parse_context(tokens);
 
   Program* program = program_new();
 
-  // TopLevel statements
-  Statement* stmt = NULL;
-  Function* fn = NULL;
-    
-  while (true) {
-    size_t lst_idx = get_ctx_idx();
-    stmt = parse_statement();
-    if (stmt) {
-      program_add_statement(program, stmt);
-      continue;
-    }   
-    // reset token
-    set_ctx_idx(lst_idx);
-
-    // failed to parse statement: 
-    // try parse function definition
-    fn = parse_function_def();
-    NULL;
-    if (fn) {
-      program_add_function(program, fn);
-      continue;
-    }
-
-    // failed to parse anything
-    break;
-  }
+  while (try_parse(program)) { }
 
   return program;
 }
 
-// add statements to a program (mainly for repl)
+// toplevel statements to a program (mainly for repl)
 bool parse_line(Program* program, Vector tokens) {
   init_parse_context(tokens);
+  return try_parse(program);
+}
 
-  Statement* stmt = parse_statement();
-  if (stmt == NULL) return false;
+static bool try_parse(Program* program) {
+  // TopLevel statements
+  Statement* stmt = NULL;
+  Function* fn = NULL;
+    
+  size_t lst_idx = get_ctx_idx();
+  stmt = parse_statement();
+  if (stmt) {
+    program_add_statement(program, stmt);
+    return true;
+  }   
+  // reset token
+  set_ctx_idx(lst_idx);
 
-  program_add_statement(program, stmt);
-  
-  return true;
+  // failed to parse statement: 
+  // try parse function definition
+  fn = parse_function_def();
+  if (fn) {
+    program_add_function(program, fn);
+    return true;
+  }
+
+  return false;
 }
 
